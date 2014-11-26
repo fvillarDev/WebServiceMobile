50ef5606020bc7276fe2f0d84402650e963984fa
fv_android <facuandroid.fv@gmail.com>
2014-11-26 17:57:53 -0300

    Add new project

---
+++ WebServiceMobile/Helper.cs
@@ -0,0 +1,296 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace WebServiceMobile
+{
+    public class Helper
+    {
+        private const string cnnString = "Server=08b75c75-cfac-4d9b-b023-a39b01057665.sqlserver.sequelizer.com;Database=db08b75c75cfac4d9bb023a39b01057665;User ID=dkeybpcggpoutvaf;Password=CJPQEYNWiXiAY5TUxzy8DHJ3sbQDHbPEGZkyK3ZrTvYnAMytZWzuzbR4aVwCiing;";
+
+        public static DataSet DsFeeds = new DataSet();
+        public static Dictionary<string, string> UserIdToken = new Dictionary<string, string>();
+        public static Dictionary<string, string> UserIdTokenToSend = new Dictionary<string, string>();
+
+        private const string API_KEY = "AIzaSyAHMcqlO5o7GrGXpf6gD_-ndY1wxLQp_t4";
+
+        public static string SendGCMNotification(string apiKey, string postData, string postDataContentType = "application/json")
+        {
+            ServicePointManager.ServerCertificateValidationCallback += new RemoteCertificateValidationCallback(ValidateServerCertificate);
+
+            //
+            //  MESSAGE CONTENT
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+            //
+            //  CREATE REQUEST
+            HttpWebRequest Request = (HttpWebRequest)WebRequest.Create("https://android.googleapis.com/gcm/send");
+            Request.Method = "POST";
+            Request.KeepAlive = false;
+            Request.ContentType = postDataContentType;
+            Request.Headers.Add(string.Format("Authorization: key={0}", apiKey));
+            Request.ContentLength = byteArray.Length;
+
+            Stream dataStream = Request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+
+            //
+            //  SEND MESSAGE
+            try
+            {
+                WebResponse Response = Request.GetResponse();
+                HttpStatusCode ResponseCode = ((HttpWebResponse)Response).StatusCode;
+                if (ResponseCode.Equals(HttpStatusCode.Unauthorized) || ResponseCode.Equals(HttpStatusCode.Forbidden))
+                {
+                    return "Unauthorized - invalid token";
+                }
+                if (!ResponseCode.Equals(HttpStatusCode.OK))
+                {
+                    return "An error ocurred processing the request";
+                }
+
+                StreamReader Reader = new StreamReader(Response.GetResponseStream());
+                string responseLine = Reader.ReadToEnd();
+                Reader.Close();
+
+                return responseLine;
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+
+        private static bool ValidateServerCertificate(
+                                            object sender,
+                                            X509Certificate certificate,
+                                            X509Chain chain,
+                                            SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+
+        public static void GCMNotification(int cant)
+        {
+            try
+            {
+                string DEVICE_TOKEN = "";
+                //SqlConnection cnn = new SqlConnection(cnnString);
+                //SqlCommand cmd = new SqlCommand("SELECT * FROM GCM_News", cnn);
+                //SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                //cnn.Open();
+                //DataSet ds = new DataSet();
+                //adapter.Fill(ds);
+                //cnn.Close();
+
+                const string message = "Nuevas Noticias Disponibles";
+                const string tickerText = "Nuevas Noticias Transporte Córdoba";
+                const string contentTitle = "Informe Transporte Cordoba";
+                string subText = cant + " noticias encontradas";
+                if (cant == 1) { subText = cant + " noticia encontrada"; }
+
+                foreach (KeyValuePair<string, string> pair in UserIdTokenToSend)
+                {
+                    DEVICE_TOKEN = pair.Value;
+                    string postData =
+                        "{ \"registration_ids\": [ \"" + DEVICE_TOKEN + "\" ], " +
+                        "\"data\": {\"tickerText\":\"" + tickerText + "\", " +
+                        "\"contentTitle\":\"" + contentTitle + "\", " +
+                        "\"subText\":\"" + subText + "\", " +
+                        "\"message\": \"" + message + "\"}}";
+
+                    SendGCMNotification(API_KEY, postData);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex.InnerException;
+            }
+        }
+
+        /// <summary>
+        /// Lee los feeds de cada url
+        /// </summary>
+        /// <returns>Cantidad de feeds validos</returns>
+        public static int ReadFeeds()
+        {
+            GetPreviousFeeds();
+            GetUserIds();
+
+            int cant = 0;
+            List<string> rssUrlsList = new List<string> { "http://www.lavoz.com.ar/rss.xml", "http://www.diaadia.com.ar/rss.xml",
+                "http://www.lmcordoba.com.ar/rss/ultimas.xml", "http://www.cba24n.com.ar/publico/tda_xml.php" }; //, "http://ersaurbano.com/feed"};
+
+            foreach (string url in rssUrlsList)
+            {
+                try
+                {
+                    string result = GetPageAsString(new Uri(url));
+                    if (string.IsNullOrEmpty(result)) continue;
+                    DataSet rssData = new DataSet();
+                    rssData.ReadXml(new StringReader(result), XmlReadMode.Auto);
+                    string rawGuid = "";
+                    if (url.Contains("lavoz") || url.Contains("cba24n"))
+                    {
+                        for (int i = 0; i < rssData.Tables["item"].Rows.Count; i++)
+                        {
+                            if (!ValidTitle(rssData.Tables["item"].Rows[i]["title"].ToString())) continue;
+                            rawGuid = rssData.Tables["guid"].Rows[i]["guid_Text"].ToString();
+                            cant++;
+                            TitleNotExists(rssData.Tables["item"].Rows[i]["title"].ToString(), rawGuid);
+                        }
+
+                    }
+                    else
+                    {
+                        cant += rssData.Tables["item"].Rows.Cast<DataRow>().Where(row => ValidTitle(row["title"].ToString())).Count(row => TitleNotExists(row["title"].ToString(), row["guid"].ToString()));
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+
+            return cant;
+        }
+
+        private static void GetPreviousFeeds()
+        {
+            SqlConnection cnn = new SqlConnection(cnnString);
+            string query = "SELECT * FROM GCM_Feeds";
+            string queryDelete = "DELETE FROM GCM_Feeds WHERE FeedDate < '" + DateTime.Now.AddDays(-7).ToString("s") + "'";
+            SqlCommand cmd = new SqlCommand(query, cnn);
+            SqlCommand cmdDelete = new SqlCommand(queryDelete, cnn);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            cnn.Open();
+            cmdDelete.ExecuteNonQuery();
+            DsFeeds = new DataSet();
+            adapter.Fill(DsFeeds);
+            cnn.Close();
+        }
+
+        private static void GetUserIds()
+        {
+            UserIdToken = new Dictionary<string, string>();
+            UserIdTokenToSend = new Dictionary<string, string>();
+            SqlConnection cnn = new SqlConnection(cnnString);
+            SqlCommand cmd = new SqlCommand("SELECT ID, GCMToken, GCMToken2 FROM GCM_News", cnn);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            cnn.Open();
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            cnn.Close();
+
+            if (ds.Tables.Count <= 0) return;
+            foreach (DataRow row in ds.Tables[0].Rows)
+            {
+                UserIdToken.Add(row[0].ToString(), row[1].ToString() + row[2].ToString());
+            }
+        }
+
+        private static bool TitleNotExists(string title, string rawGuid)
+        {
+            string guid = GetGuidFromRaw(rawGuid);
+            if (DsFeeds.Tables.Count <= 0)
+            {
+                UserIdTokenToSend = UserIdToken;
+                InsertFeed(title, guid);
+                return true;
+            }
+
+            if (DsFeeds.Tables[0].Rows.Count > 0)
+            {
+                bool equals = DsFeeds.Tables[0].Rows.Cast<DataRow>().Any(row => row[3].ToString().Equals(guid));
+
+                if (equals)
+                {
+                    string[] ids = DsFeeds.Tables[0].Select("Guid='" + guid + "'")[0][2].ToString().Split(',');
+                    foreach (string id in ids.Where(id => !UserIdToken.ContainsKey(id)))
+                    {
+                        string val = "";
+                        UserIdTokenToSend.TryGetValue(id, out val);
+                        if (!string.IsNullOrEmpty(val))
+                            UserIdTokenToSend.Add(id, val);
+                    }
+
+                    UpdateFeedIds(guid);
+                    return true;
+                }
+            }
+
+            UserIdTokenToSend = UserIdToken;
+            InsertFeed(title, guid);
+            return true;
+        }
+
+        private static void InsertFeed(string title, string guid)
+        {
+            SqlConnection cnn = new SqlConnection(cnnString);
+            string ids = String.Join(",", UserIdToken.Keys.ToArray());
+            string query = "INSERT INTO GCM_Feeds VALUES('" + title + "', '" + DateTime.Now.ToString("s") + "', '" + ids + "', '" + guid + "')";
+            SqlCommand cmd = new SqlCommand(query, cnn);
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+        private static void UpdateFeedIds(string guid)
+        {
+            SqlConnection cnn = new SqlConnection(cnnString);
+            string ids = String.Join(",", UserIdToken.Keys.ToArray());
+            ids += "," + String.Join(",", UserIdTokenToSend.Keys.ToArray());
+            if (ids.EndsWith(","))
+            {
+                ids = ids.Substring(0, ids.Length - 1);
+            }
+            string query = "UPDATE GCM_Feeds SET UserId='" + ids + "' WHERE Guid='" + guid + "'";
+            SqlCommand cmd = new SqlCommand(query, cnn);
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+        private static string GetPageAsString(Uri address)
+        {
+            string result = "";
+            HttpWebRequest request = WebRequest.Create(address) as HttpWebRequest;
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                result = reader.ReadToEnd();
+            }
+            return result;
+        }
+
+        private static bool ValidTitle(string title)
+        {
+            if (!title.Contains("paro") && !title.Contains("Paro")) return false;
+            return title.Contains("transporte") || title.Contains("UTA") || title.Contains("colectivo") ||
+                   title.Contains("colectivos") || title.Contains("choferes")
+                   || title.Contains("chofer") || title.Contains("transportes");
+        }
+
+        private static string GetGuidFromRaw(string rawGuid)
+        {
+            if (rawGuid.Contains("lavoz") || rawGuid.Contains("diaadia"))
+            {
+                return rawGuid.Split(' ')[0];
+            }
+            if (rawGuid.Contains("lmcordoba"))
+            {
+                return rawGuid.Replace("http://www.lmcordoba.com.ar/nota.php?ni=", "");
+            }
+            return rawGuid.Contains("cba24n") ? rawGuid.Replace("http://www.cba24n.com.ar/node/", "") : "";
+        }
+    }
+}
---
+++ WebServiceMobile/ServiceMobile.asmx.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.Services;
+
+namespace WebServiceMobile
+{
+    [WebService(Namespace = "http://androiddev.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    [System.Web.Script.Services.ScriptService]
+    public class ServiceMobile : WebService
+    {
+
+        #region Properties
+        private string idGeo = "dd540f1f-44ac-4929-a4e8-777a5d9b66b3";
+        private string key = "n9tNJicGUqLc6KbDzeGoVNoDcNC70rjEgrXrKM8a";
+        string BASE_URL = "http://ws_geosolution.geosolution.com.ar/mobile_test/Mobile/";
+        private const string cnnString = "Server=08b75c75-cfac-4d9b-b023-a39b01057665.sqlserver.sequelizer.com;Database=db08b75c75cfac4d9bb023a39b01057665;User ID=dkeybpcggpoutvaf;Password=CJPQEYNWiXiAY5TUxzy8DHJ3sbQDHbPEGZkyK3ZrTvYnAMytZWzuzbR4aVwCiing;";
+        #endregion
+
+        #region Web Methods
+
+        [WebMethod]
+        public string GetHourMobile(string stop, string bus)
+        {
+            bool isNoData = true;
+            string res = "";
+            while (isNoData)
+            {
+                res = GetData(stop, bus);
+                if (res.Contains("ErrorMessage") && res.Contains("nivel"))
+                { }
+                else
+                    isNoData = false;
+            }
+            return res;
+        }
+
+        [WebMethod]
+        public string GetNearStopsByBus(string bus, string lat, string lng)
+        {
+            bool isNoData = true;
+            string res = "";
+            while (isNoData)
+            {
+                res = StopsByBus(bus, lat, lng);
+                if (res.Contains("ErrorMessage") && res.Contains("nivel"))
+                { }
+                else
+                    isNoData = false;
+            }
+            return res;
+        }
+
+        [WebMethod]
+        public string GetPositionByStop(string stop)
+        {
+            bool isNoData = true;
+            string res = "";
+            while (isNoData)
+            {
+                res = GetParadaStop(stop);
+                if (res.Contains("ErrorMessage") && res.Contains("nivel"))
+                { }
+                else
+                    isNoData = false;
+            }
+            return res;
+        }
+
+        [WebMethod]
+        public void GetFeeds()
+        {
+            int cant = Helper.ReadFeeds();
+            if (cant > 0)
+            {
+                Helper.GCMNotification(cant);
+            }
+        }
+
+        [WebMethod]
+        public string AddNewGCMToken(string token)
+        {
+            try
+            {
+                if (token.Length < 129) return "Error. Invalid Token";
+                string tkn1 = token.Substring(0, 128);
+                string tkn2 = token.Replace(tkn1, "");
+
+                SqlConnection cnn = new SqlConnection(cnnString);
+                SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM GCM_News WHERE GCMToken='" + tkn1 + "' AND GCMToken2='" + tkn2 + "' " +
+                                                "SELECT MAX(ID) FROM GCM_News", cnn);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                cnn.Open();
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                int maxId = 1;
+
+                if (ds.Tables.Count > 0)
+                {
+                    if (ds.Tables[1].Rows.Count > 0)
+                    {
+                        if (!string.IsNullOrEmpty(ds.Tables[1].Rows[0][0].ToString()))
+                        {
+                            maxId = Int32.Parse(ds.Tables[1].Rows[0][0].ToString());
+                            maxId++;
+                        }
+                    }
+
+                    if (ds.Tables[0].Rows.Count > 0)
+                    {
+                        if (!string.IsNullOrEmpty(ds.Tables[0].Rows[0][0].ToString()))
+                        {
+                            int exite = Int32.Parse(ds.Tables[0].Rows[0][0].ToString());
+                            if (exite != 0) return "Already Exists";
+                        }
+                    }
+                }
+                else return "Error";
+
+
+                string query = "INSERT INTO GCM_News VALUES(" + maxId + ",'" + tkn1 + "', '" + Guid.NewGuid().ToString() + "', '" + DateTime.Now.ToString("s") + "', '" + tkn2 + "')";
+                SqlCommand cmd2 = new SqlCommand(query, cnn);
+                cmd2.ExecuteNonQuery();
+                cnn.Close();
+
+                return "OK" + maxId;
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        [WebMethod]
+        public string DeleteToken(string token)
+        {
+            try
+            {
+                SqlConnection cnn = new SqlConnection(cnnString);
+                SqlCommand cmd =
+                    new SqlCommand("DELETE FROM GCM_News WHERE ID='" + token + "'",
+                        cnn);
+                cnn.Open();
+                cmd.ExecuteNonQuery();
+                cnn.Close();
+                return "OK";
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private string GetData(string parada, string linea)
+        {
+            DateTime date = DateTime.Now;
+            string dayToSend = date.ToString("yyyy-MM-ddThh:mm:ss");
+            string mensajeFecha = date.ToString("yyyyMMddhhmmss");
+            string hashBuild = "";
+            string mensaje = linea + parada + mensajeFecha;
+
+            byte[] keyArray;
+            using (HMACMD5 m = new HMACMD5(UTF8Encoding.UTF8.GetBytes(key)))
+            {
+                keyArray = m.ComputeHash(UTF8Encoding.UTF8.GetBytes(mensaje));
+            }
+
+            hashBuild = Convert.ToBase64String(keyArray, 0, keyArray.Length);
+
+            string uri = BASE_URL + "CalcularMobile?" +
+                         "parada=" + parada + "&linea=" + linea +
+                         "&id=" + idGeo +
+                         "&hash=" + hashBuild + "&fecha=" + dayToSend;
+
+            WebRequest req = WebRequest.Create(uri);
+            WebResponse resp = req.GetResponse();
+
+            StreamReader sr = new StreamReader(resp.GetResponseStream());
+            return sr.ReadToEnd().Trim();
+        }
+
+        private string StopsByBus(string linea, string lat, string lng)
+        {
+            DateTime date = DateTime.Now;
+            string dayToSend = date.ToString("yyyy-MM-ddThh:mm:ss");
+            string mensajeFecha = date.ToString("yyyyMMddhhmmss");
+            string hashBuild = "";
+            byte[] keyArray;
+            using (HMACMD5 m = new HMACMD5(UTF8Encoding.UTF8.GetBytes(key)))
+            {
+                keyArray = m.ComputeHash(UTF8Encoding.UTF8.GetBytes(mensajeFecha));
+            }
+
+            hashBuild = Convert.ToBase64String(keyArray, 0, keyArray.Length);
+
+            string uri = BASE_URL + "ObtenerParadasCercanasPorLinea?" +
+                         "linea=" + linea +
+                         "&longitud=" + lng + "&latitud=" + lat +
+                         "&id=" + idGeo +
+                         "&hash=" + hashBuild + "&fecha=" + dayToSend;
+
+            WebRequest req = WebRequest.Create(uri);
+            WebResponse resp = req.GetResponse();
+
+            StreamReader sr = new StreamReader(resp.GetResponseStream());
+            return sr.ReadToEnd().Trim();
+        }
+
+        private string GetParadaStop(string parada)
+        {
+            DateTime date = DateTime.Now;
+            string dayToSend = date.ToString("yyyy-MM-ddThh:mm:ss");
+            string mensajeFecha = date.ToString("yyyyMMddhhmmss");
+            string hashBuild = "";
+            byte[] keyArray;
+            using (HMACMD5 m = new HMACMD5(UTF8Encoding.UTF8.GetBytes(key)))
+            {
+                keyArray = m.ComputeHash(UTF8Encoding.UTF8.GetBytes(mensajeFecha));
+            }
+
+            hashBuild = Convert.ToBase64String(keyArray, 0, keyArray.Length);
+
+            string uri = BASE_URL + "ObtenerParada?" +
+                         "parada=" + parada +
+                         "&id=" + idGeo +
+                         "&hash=" + hashBuild + "&fecha=" + dayToSend;
+
+            WebRequest req = WebRequest.Create(uri);
+            WebResponse resp = req.GetResponse();
+
+            StreamReader sr = new StreamReader(resp.GetResponseStream());
+            return sr.ReadToEnd().Trim();
+        }
+
+        #endregion
+    }
+}
